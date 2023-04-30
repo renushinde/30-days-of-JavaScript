05a3f66b917e3dbae65d4cdbf6046391f91ccf0f
yummygoods <melissa@yummygoods.com>
2023-04-30 10:38:32 -0400

    complete exercises Day 1

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,6 @@
 console.log('Hello, World');
+// Day 1 Exercises
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*Write a multiline comment which says, comments can make code readable, easy to reuse and informative*/
+
---
+++ variables.js
@@ -0,0 +1,31 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// 6. Declare four variables without assigning values
+let monkey;
+let a;
+let answer;
+let fingers;
+
+// 7. Declare four variables with assigned values
+let pusheen = "i am pusheen the cat";
+let tinaBelcher = "i will see you in hell";
+let alexisRose = "ew, david!";
+let luckyNumber = 13;
+let isSunny = false;
+
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Melissa";
+let lastName = "Averinos";
+let maritalStatus = "married";
+let country = "United States";
+let age = 48;
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+//nah
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 48;
+let yourAge = 62;
+console.log(myAge, yourAge);
