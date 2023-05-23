48d5a3e0af65c3f34f78cb217ddb8a30a1f6f5a5
yummygoods <melissa@yummygoods.com>
2023-05-22 22:01:23 -0400

    settings change

--- melissa/day5arrays.js
+++ melissa/day5arrays.js
@@ -1,26 +1,26 @@
 const countries = [
-  'Albania',
-  'Bolivia',
-  'Canada',
-  'Denmark',
-  'Ethiopia',
-  'Finland',
-  'Germany',
-  'Hungary',
-  'Ireland',
-  'Japan',
-  'Kenya'
-]
+	'Albania',
+	'Bolivia',
+	'Canada',
+	'Denmark',
+	'Ethiopia',
+	'Finland',
+	'Germany',
+	'Hungary',
+	'Ireland',
+	'Japan',
+	'Kenya',
+];
 
 const webTechs = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Redux',
-  'Node',
-  'MongoDB'
-]
+	'HTML',
+	'CSS',
+	'JavaScript',
+	'React',
+	'Redux',
+	'Node',
+	'MongoDB',
+];
 
 // Declare an empty array;
 const arr = Array();
@@ -29,7 +29,7 @@ console.log(colors);
 console.log(Array.isArray(colors));
 
 // Declare an array with more than 5 number of elements
-let array = []
+let array = [];
 // Find the length of your array
 // Get the first item, the middle item and the last item of the array
 // Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
@@ -52,25 +52,35 @@ let array = []
 // Remove the last IT company from the array
 // Remove all IT companies
 
-
-
 // Exercise: Level 2
 //1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
 //2. First remove all the punctuations and change the string to array and count the number of words in the array
 
 let text =
-'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-console.log(words)
-console.log(words.length)
+	'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+console.log(words);
+console.log(words.length)[
+	('I',
+	'love',
+	'teaching',
+	'and',
+	'empowering',
+	'people',
+	'I',
+	'teach',
+	'HTML',
+	'CSS',
+	'JS',
+	'React',
+	'Python')
+];
 
-["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
-
-13
+13;
 
 //3. In the following shopping cart add, remove, edit items
 
- const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
 // add 'Meat' in the beginning of your shopping cart if it has not been already added
 // add Sugar at the end of you shopping cart if it has not been already added
 // remove 'Honey' if you are allergic to honey
@@ -82,25 +92,25 @@ console.log(words.length)
 
 //6. Concatenate the following two variables and store it in a fullStack variable.
 
-const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-const backEnd = ['Node','Express', 'MongoDB']
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node', 'Express', 'MongoDB'];
 
-console.log(fullStack)
-["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+console.log(fullStack)[
+	('HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB')
+];
 
 // Exercise: Level 3
 // The following is an array of 10 students ages:
 
-const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
 // Sort the array and find the min and max age
 // Find the median age(one middle item or two middle items divided by two)
 // Find the average age(all items divided by number of items)
 // Find the range of the ages(max minus min)
-// Compare the value of (min - average) and (max - average), use abs() method 
+// Compare the value of (min - average) and (max - average), use abs() method
 
 // 1. Slice the first ten countries from the countries array
 // 2. Find the middle country(ies) in the countries array
 
 // 3. Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
-
