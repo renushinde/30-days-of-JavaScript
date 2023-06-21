dd9891dbc6f33f5d045eb70a87cd38a861694c50
yummygoods <melissa@yummygoods.com>
2023-06-21 11:26:26 -0400

    more Maps and Sets practice

--- melissa/day10MapSet.js
+++ melissa/day10MapSet.js
@@ -1,63 +1,91 @@
 // Exercises:Level 1
 
 
-// create an empty set
-const set1 = new Set();
-console.log(set1);
-// Create a set containing 0 to 10 using loop
-const numSet = new Set();
-for(let n = 0; n<=10; n++ ){
-  numSet.add(n);
-}
-console.log(numSet);
+// // create an empty set
+// const set1 = new Set();
+// console.log(set1);
+// // Create a set containing 0 to 10 using loop
+// const numSet = new Set();
+// for(let n = 0; n<=10; n++ ){
+//   numSet.add(n);
+// }
+// console.log(numSet);
 
 
-// Remove an element from a set
-numSet.delete(5);
-console.log(numSet);
-// Clear a set
-numSet.clear();
-console.log(numSet);
-// Create a set of 5 string elements from array
-const pets = ["max", "beau", "blue", "riley", "zeus", "max", "blue"];
-const petSet = new Set(pets);
-console.log(petSet);
+// // Remove an element from a set
+// numSet.delete(5);
+// console.log(numSet);
+// // Clear a set
+// numSet.clear();
+// console.log(numSet);
+// // Create a set of 5 string elements from array
+// const pets = ["max", "beau", "blue", "riley", "zeus", "max", "blue"];
+// const petSet = new Set(pets);
+// console.log(petSet);
 
-// Create a map of countries and number of characters of a country
-const countries = ['Finland', 'Sweden', 'Norway']
-const countriesWithLength = countries.map((country) => [country, country.length] )
+// // Create a map of countries and number of characters of a country
+// const countries = ['Finland', 'Sweden', 'Norway']
+// const countriesWithLength = countries.map((country) => [country, country.length] )
 
-console.log(countriesWithLength);
-const newMap = new Map(countriesWithLength);
+// console.log(countriesWithLength);
+// const newMap = new Map(countriesWithLength);
 
-console.log(newMap);
-
-
-// Exercises:Level 2
-
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
+// console.log(newMap);
 
 
-// Find a union b
-const newC = [...a, ...b]
-console.log(newC)
-const unionC = new Set(newC)
-console.log(unionC);
+// // Exercises:Level 2
+
+// const a = [4, 5, 8, 9]
+// const b = [3, 4, 5, 7]
 
 
-// Find a intersection b
+// // Find a union b
+// const newC = [...a, ...b]
+// console.log(newC)
+// const unionC = new Set(newC)
+// console.log(unionC);
 
-const B = new Set (b);
-let intersection = a.filter(item => B.has(item))
-console.log(intersection);
+
+// // Find a intersection b
+
+// const B = new Set (b);
+// let intersection = a.filter(item => B.has(item))
+// console.log(intersection);
 
 // Find a with b
 
 // Exercises:Level 3
 // How many languages are there in the countries object file.
 
+// console.log(countries);
+let langArray = [];
+for(country of countries){
+  langArray = [...country.languages, ...langArray]
+}
+
+
+console.log(langArray.length);
+// console.log("lang array" + langArray);
+const unique = new Set(langArray);
+console.log(unique);
+
 // *** Use the countries data to find the 10 most spoken languages:
+
+const result = langArray.reduce(accum, item => {
+  ![item] ? [item] = accum : [item] = accum++;
+}
+  ,{}) ;
+
+  console.log(result);
+
+//start with empty object
+//loop though (reduce)  array 
+// item does not exist? create it
+//does exist, update count 
+//return
+
+
+
 
    // Your output should look like this
   //  console.log(mostSpokenLanguages(countries, 10))
